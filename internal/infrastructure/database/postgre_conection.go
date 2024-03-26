7f6a4f37f3218d6f212859f84351360ba4ea069b
Janduy Monroe <janduymonroe@gmail.com>
2024-03-26 20:22:50 -0300

    fix: ajustado acesso ao banco

--- internal/infrastructure/database/postgre_conection.go
+++ internal/infrastructure/database/postgre_conection.go
@@ -7,12 +7,12 @@ import (
 )
 
 func NewPostgresDB() *gorm.DB {
-	host := "lanchonetedaruadb.czcuc4gason0.us-east-1.rds.amazonaws.com"
+	host := "lanchonetedaruadb.c16om6u44j69.us-east-1.rds.amazonaws.com"
 	port := 5432
 	user := "postgres"
 	password := "QE1muGg0fwsepsH"
 	dbname := "cliente"
-	sslMode := "disable"
+	sslMode := "require"
 	timeZone := "America/Sao_Paulo"
 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s TimeZone=%s", host, port, user, password, dbname, sslMode, timeZone)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
